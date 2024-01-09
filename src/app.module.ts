b2cece96b607529cf6a7177a9e589b82ea428d2a
sh940701 <visioner2168@gmail.com>
2024-01-09 11:40:42 +0900

    update: add autoSchemaFile option

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
 
 @Module({
     imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver
+        GraphQLModule.forRoot({
+            autoSchemaFile: 'schema.gql'
         })
     ],
     controllers: [AppController],
