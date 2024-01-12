1c815e6e6888ee2cca20013b86a84e5f24500c05
sh940701 <visioner2168@gmail.com>
2024-01-12 14:36:52 +0900

    feat: service application configuration

--- apps/my-mono-app/src/main.ts
+++ apps/my-mono-app/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { MyMonoAppModule } from './my-mono-app.module';
+import {NestFactory} from '@nestjs/core';
+import {MyMonoAppModule} from './my-mono-app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {join} from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(MyMonoAppModule);
-  await app.listen(3000);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(MyMonoAppModule, {
+        transport: Transport.GRPC,
+        options: {
+            package: 'user',
+            protoPath: join(__dirname, 'user.proto')
+        }
+    });
+    await app.listen();
 }
-bootstrap();
+
+bootstrap().catch(console.error);
