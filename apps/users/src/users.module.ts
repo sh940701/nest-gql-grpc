f868facc01845f9e40c95c9ddbe5aa9be0b49874
sh940701 <visioner2168@gmail.com>
2024-01-14 00:44:38 +0900

    update: users module

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import {Module} from '@nestjs/common';
+import {UsersController} from './users.controller';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService],
+    imports: [],
+    controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule {
+}
