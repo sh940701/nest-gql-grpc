349629d8af56f693d42d698edecdea66227f10e4
sh940701 <visioner2168@gmail.com>
2024-01-14 00:44:06 +0900

    update: add url property to server instance

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,14 +1,17 @@
 import {NestFactory} from '@nestjs/core';
 import {MicroserviceOptions, Transport} from "@nestjs/microservices";
-import {AppModule} from "../../nest-gql/src/app.module";
 import {join} from 'path'
+import {UsersModule} from "./users.module";
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(UsersModule, {
         transport: Transport.GRPC,
         options: {
             package: 'users',
-            protoPath: join(__dirname, 'users/user.proto')
+            protoPath: join(__dirname, 'user.proto'),
+            // url 의 경우 local 에서 실행할 때는 설정해주지 않아도 된다.
+            // 그러나 docker compose 등으로 실행하거나, 외부에서의 grpc 테스트를 위해선 명시해줄 필요가 있다.
+            url: '0.0.0.0:50001'
         }
     });
     await app.listen();
