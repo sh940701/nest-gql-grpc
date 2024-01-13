8abc61514434c746ed6294769f3d8fb707b0d411
sh940701 <visioner2168@gmail.com>
2024-01-14 00:45:26 +0900

    update: grpc method setting

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,17 +1,25 @@
 import {Controller} from '@nestjs/common';
-import {UsersService} from './users.service';
 import {GrpcMethod} from "@nestjs/microservices";
 import {Metadata, ServerUnaryCall} from "@grpc/grpc-js";
 
+export interface UserById {
+    id: number
+}
+
+export interface User {
+    id: number
+    name: string
+}
+
 @Controller()
 export class UsersController {
-    @GrpcMethod('UsersService', 'FindOne')
-    findOne(data: UserById, metadata: Metadata, call: ServerUnaryCall<any, any>): User {
+    @GrpcMethod('UsersService')
+    findOne(data: UserById, metadata?: Metadata, call?: ServerUnaryCall<any, any>) {
         const items = [
             {id: 1, name: 'John'},
             {id: 2, name: 'Do'}
         ]
 
-        return items.find(({id}) => id === data.id)
+        return items.find(({id}) => id === data.id) as any
     }
 }
