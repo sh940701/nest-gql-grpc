f4aa184adbee56b6ad73bbdef966062208be132e
sh940701 <visioner2168@gmail.com>
2024-01-13 17:46:44 +0900

    update: users service module

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { UsersModule } from './users.module';
+import {NestFactory} from '@nestjs/core';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {AppModule} from "../../nest-gql/src/app.module";
+import {join} from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
-  await app.listen(3000);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.GRPC,
+        options: {
+            package: 'users',
+            protoPath: join(__dirname, 'users/user.proto')
+        }
+    });
+    await app.listen();
 }
+
 bootstrap();
