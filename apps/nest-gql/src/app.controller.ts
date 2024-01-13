ebbd7636bc05852b0e187080a0f8a881ff2b8e02
sh940701 <visioner2168@gmail.com>
2024-01-14 00:43:16 +0900

    feat: add get users path

--- apps/nest-gql/src/app.controller.ts
+++ apps/nest-gql/src/app.controller.ts
@@ -1,9 +1,15 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
 import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {
+    }
+
+    @Get('/users/:_id')
+    getUser(@Param('_id') id: string): Promise<any> {
+        console.log(id)
+        return this.appService.getUser({id: Number(id)})
     }
 
     @Get()
