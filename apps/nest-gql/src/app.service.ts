85b05b8da44232974e277eb733b688b1a49673a7
sh940701 <visioner2168@gmail.com>
2024-01-14 00:43:48 +0900

    feat: add setting for call grpc method

--- apps/nest-gql/src/app.service.ts
+++ apps/nest-gql/src/app.service.ts
@@ -1,8 +1,34 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable, OnModuleInit} from '@nestjs/common';
+import {UserById, UsersController} from "../../users/src/users.controller";
+import {ClientGrpc} from "@nestjs/microservices";
 
 @Injectable()
-export class AppService {
-    getHello(): string {
-        return 'Hello World!';
+export class AppService implements OnModuleInit {
+    // 아래와 같이 service layer 에서 세팅해줘도 됨
+    // @Client({
+    //     transport: Transport.GRPC,
+    //     options: {
+    //         package: 'users',
+    //         protoPath: join(__dirname, '../users/user.proto'),
+    //         // url: '0.0.0.0:50001'
+    //     }
+    // })
+    // private client: ClientGrpc
+
+    private usersController: UsersController
+
+    constructor(@Inject('USERS_PACKAGE') private client: ClientGrpc) {
+    }
+
+    onModuleInit() {
+        this.usersController = this.client.getService<UsersController>('UsersService')
+    }
+
+    async getUser(data: UserById) {
+        return this.usersController.findOne(data)
+    }
+
+    getHello() {
+        return 'hello, world'
     }
 }
