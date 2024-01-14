545d425a1f4502ffcbb22099d7f7994eb4e0ebc8
sh940701 <visioner2168@gmail.com>
2024-01-14 14:39:22 +0900

    update: customer service to use grpc service

--- apps/nest-gql/src/customer/customer.service.ts
+++ apps/nest-gql/src/customer/customer.service.ts
@@ -1,16 +1,37 @@
-import {Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
+import {Inject, Injectable} from "@nestjs/common";
 import {CustomerModel} from "./customer.model";
-import {Repository} from "typeorm";
 import {CustomerDTO} from "./customer.dto";
+import {UserById, UsersController} from "../../../users/src/users.controller";
+import {ClientGrpc} from "@nestjs/microservices";
 
 @Injectable()
 export class CustomerService {
-    constructor(
-        @InjectRepository(CustomerModel)
-        private customerRepository: Repository<CustomerModel>
-    ) {
+    private usersController: UsersController
+
+    constructor(@Inject("USERS_PACKAGE") private client: ClientGrpc) {
     }
+
+    onModuleInit() {
+        this.usersController = this.client.getService<UsersController>('UsersService')
+    }
+
+    getUsers() {
+        return this.usersController.findAll({})
+    }
+
+    getUser(data: UserById) {
+        return this.usersController.findOne(data)
+    }
+
+    createUser(data) {
+        return this.usersController.insertOne(data)
+    }
+
+    // constructor(
+    //     @InjectRepository(CustomerModel)
+    //     private customerRepository: Repository<CustomerModel>
+    // ) {
+    // }
 
     create(details: CustomerDTO): Promise<CustomerModel> {
         return this.customerRepository.save(details)
