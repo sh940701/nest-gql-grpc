f0634697b0c0cdfb1ca24f6ca0080bc804497520
sh940701 <visioner2168@gmail.com>
2024-01-21 13:49:49 +0900

    feat: register client GRPC server

--- apps/nest-gql/src/customer/customer.module.ts
+++ apps/nest-gql/src/customer/customer.module.ts
@@ -1,11 +1,25 @@
 import {Module} from "@nestjs/common";
 import {CustomerService} from "./customer.service";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {CustomerModel} from "./customer.model";
 import {CustomerResolver} from "./customer.resolver";
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {join} from "path";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([CustomerModel])],
+    imports: [
+        ClientsModule.register([
+            {
+                name: 'USERS_PACKAGE',
+                transport: Transport.GRPC,
+                options: {
+                    package: "users",
+                    protoPath: join(__dirname, '../users/user.proto'),
+                    // url 의 경우 local 에서 실행할 때는 설정해주지 않아도 된다.
+                    // 그러나 docker compose 등으로 실행하거나, 외부에서의 grpc 테스트를 위해선 명시해줄 필요가 있다.
+                    url: '0.0.0.0:50001'
+                }
+            }
+        ]),
+    ],
     providers: [CustomerService, CustomerResolver]
 })
 export class CustomerModule {
