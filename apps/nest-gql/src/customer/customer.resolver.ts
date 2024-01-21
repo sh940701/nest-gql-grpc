8708689afc720c23e81731d8a29497f5b3d588ba
sh940701 <visioner2168@gmail.com>
2024-01-21 13:47:55 +0900

    feat: add features for graphql resolver

--- apps/nest-gql/src/customer/customer.resolver.ts
+++ apps/nest-gql/src/customer/customer.resolver.ts
@@ -1,33 +1,41 @@
 import {Args, Mutation, Query, Resolver} from "@nestjs/graphql";
-import {CustomerModel} from "./customer.model";
 import {Inject} from "@nestjs/common";
 import {CustomerService} from "./customer.service";
+import {User} from "./customer.model";
+import {CreateUserDTO} from "../../../users/src/entities/user.entity";
 
-@Resolver(() => CustomerModel)
+@Resolver(() => User)
 export class CustomerResolver {
     constructor(
         @Inject(CustomerService) private customerService: CustomerService,
     ) {
     }
 
-    @Query(() => CustomerModel)
-    async customer(@Args('id') id: string): Promise<CustomerModel> {
-        return await this.customerService.findOne(id)
+    @Query(() => User)
+    user(@Args('id') id: string) {
+        return this.customerService.getUser({id})
     }
 
 
-    @Query(() => [CustomerModel])
-    async customers(): Promise<CustomerModel[]> {
-        return await this.customerService.findAll()
+    @Query(() => [User])
+    async users() {
+        // console.log(data)
+        return await this.customerService.getUsers()
     }
 
-    @Mutation(() => CustomerModel)
-    async createCustomer(
+    @Mutation(() => User)
+    createUser(
         @Args('name') name: string,
         @Args('email') email: string,
         @Args('phone', {nullable: true}) phone: string,
         @Args('address', {nullable: true}) address: string
-    ): Promise<CustomerModel> {
-        return await this.customerService.create({name, email, phone, address})
+    ) {
+        const userData = new CreateUserDTO()
+        userData.name = name
+        userData.email = email
+        userData.phone = phone
+        userData.address = address
+
+        return this.customerService.createUser(userData)
     }
 }
